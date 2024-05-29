d71bdd263570389be585e7eb28b01744b17749dc
Nicholas Shindler <n.j.shindler@mapxact.com>
2024-05-29 13:23:18 +0200

    check is sys is windows when testing uds

--- test/integration.test.jl
+++ test/integration.test.jl
@@ -31,34 +31,36 @@ cb(args...) = nothing
     @test !isopen(server)
 end
 
-@testset "UDS Client" begin
-    ## UDS Basic Run
-    server = MQTTClient.MockMQTTBroker("/tmp/testmqtt.sock")
-    client, conn = MakeConnection("/tmp/testmqtt.sock")
+if !Sys.iswindows() # since windows is not UNIX it doesn't support UDS 
+    @testset "UDS Client" begin
+        ## UDS Basic Run
+        server = MQTTClient.MockMQTTBroker("/tmp/testmqtt.sock")
+        client, conn = MakeConnection("/tmp/testmqtt.sock")
 
-    connect(client, conn)
-    @test MQTTClient.isconnected(client)
+        connect(client, conn)
+        @test MQTTClient.isconnected(client)
 
-    res = subscribe(client, "foo/bar", cb)
-    @test res == [0x01, 0x00]
-    res = publish(client, "bar/foo", "baz")
-    @test res == 0
-    res = unsubscribe(client, "foo/bar")
-    @test isnothing(res)
+        res = subscribe(client, "foo/bar", cb)
+        @test res == [0x01, 0x00]
+        res = publish(client, "bar/foo", "baz")
+        @test res == 0
+        res = unsubscribe(client, "foo/bar")
+        @test isnothing(res)
 
-    res = disconnect(client)
-    @test res == (0x00, 0x00, 0x00)
+        res = disconnect(client)
+        @test res == (0x00, 0x00, 0x00)
 
-    # test reconnect
-    connect(client, conn)
-    @test MQTTClient.isconnected(client)
-    res = disconnect(client)
-    @test res == (0x00, 0x00, 0x00)
+        # test reconnect
+        connect(client, conn)
+        @test MQTTClient.isconnected(client)
+        res = disconnect(client)
+        @test res == (0x00, 0x00, 0x00)
 
-    @test MQTTClient.isclosed(client)
-    @test isopen(server)
+        @test MQTTClient.isclosed(client)
+        @test isopen(server)
 
-    close(server) # stop the mock server
+        close(server) # stop the mock server
 
-    @test !isopen(server)
+        @test !isopen(server)
+    end
 end
